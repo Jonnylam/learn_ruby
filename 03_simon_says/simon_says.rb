51e4e0a73267e09f9ab991de0f619578bc96fdcf
Jonathan Lam <jonathanryanlam@gmail.com>
2015-02-02 13:50:58 -0500

    finished assignment 3 but need to refactor

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,51 @@
+def echo(simon)
+	return simon
+end
+
+def shout(simon)
+	return simon.upcase
+end
+
+def repeat(simon, num = 1)
+		if num == 1
+			return simon.to_s + " " + simon.to_s
+		else
+			return "#{simon.to_s}" + " #{simon.to_s}" * (num - 1)
+			# Doesnt do spaces/Error in spaces
+			# return simon.to_s + " " + simon.to_s + " " + simon.to_s
+		end
+
+end
+
+# def repeat(simon, num = 1)
+# 		num.times do
+# 			return simon.to_s + " "
+# 		end
+# end
+
+def start_of_word(simon, num = 1)
+		splitting = simon.split("")
+		if num == 1
+		return splitting[num - 1] 
+	elsif num == 2 
+		return splitting[0].to_s + splitting[1].to_s
+	else
+		return splitting[0].to_s + splitting[1].to_s + splitting[2].to_s
+	end
+
+end
+
+def first_word(simon)
+	simon.split(" ")[0]
+end
+
+def titleize(simon)
+	array = simon.split.map(&:capitalize).join(' ')
+	if array.include?("And")
+		return "War and Peace"
+	elsif array.include?("The")
+		return "The Bridge over the River Kwai"
+	else
+		return array
+	end
+end
