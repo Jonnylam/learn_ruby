74179279f22f2e1ea2d17a004ac90fda87fa8d57
Jonathan Lam <jonathanryanlam@gmail.com>
2015-02-06 16:12:03 -0500

    didnt get the last timer

--- 08_timer/timer.rb
+++ 08_timer/timer.rb
@@ -1,8 +1,24 @@
 class Timer
-	def initialize(timer)
-		timer = @timer
+	attr_accessor :seconds
+	def initialize
+		@seconds = 0
 	end
 
-	def seconds(time = 0)
+	def time_string
+		if @seconds == 0 
+			return "00:00:00"
+		elsif @seconds <= 60
+			return "00:00:#{seconds}"
+		elsif @seconds >= 60 
+			return "00:0#{seconds.to_i/60}:0#{seconds%60}"
+		elsif @seconds >= 1200
+			return "0#{seconds.to_i/3600}:0#{seconds.to_i/3600%60}:0#{seconds%60}"
+
+		end
 	end
+
+
 end
+
+# when @seconds > 1200
+# 			"0#{@seconds/3600}:0#{@seconds%3600/60}:#{@seconds%60}"
